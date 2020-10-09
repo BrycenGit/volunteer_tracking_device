c8a2a58f0967076197834d49492100f50f13b1fd
Brycen Bartolome <brycenbartolome@gmail.com>
2020-10-09 09:04:50 -0700

    pass test, is empty to start

--- lib/project.rb
+++ lib/project.rb
@@ -11,5 +11,15 @@ class Project
     self.title == param.title && self.id == param.id
   end
 
-  
+  def self.all
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each do |project|
+      title = project.fetch("title")
+      id = project.fetch{"id"}
+      projects << Project.new({:title => title, :id => id})
+    end
+    projects
+  end
+
 end
