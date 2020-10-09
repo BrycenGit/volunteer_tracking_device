1b69861fef387f141c616e770802469120976aa7
Brycen Bartolome <brycenbartolome@gmail.com>
2020-10-09 13:51:42 -0700

    add form for volunteer

--- app.rb
+++ app.rb
@@ -10,4 +10,18 @@ also_reload('lib/**/*.rb')
 
 DB = PG.connect({:dbname => "volunteer_tracker", :password => ENV['PG_PASS']})
 
+get('/') do
+  redirect to('/volunteers')
+end
+
+get('/volunteers') do
+  @volunteers = Volunteer.all
+  erb(:volunteers)
+end
+
+get('/projects') do
+  @projects = Project.all
+  erb(:projects)
+end
+
 
