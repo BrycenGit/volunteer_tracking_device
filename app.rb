8dc918a14b7b16b9b6f6ce7b9eab2c68d9ef0459
Brycen Bartolome <brycenbartolome@gmail.com>
2020-10-09 08:18:26 -0700

    add spec helper file

--- app.rb
+++ app.rb
@@ -1,1 +1,13 @@
 #!/usr/bin/env ruby
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/volunteer')
+require('./lib/project')
+require('pry')
+require("pg")
+require('dotenv/load')
+also_reload('lib/**/*.rb')
+
+DB = PG.connect({:dbname => "", :password => ENV['PG_PASS']})
+
+
